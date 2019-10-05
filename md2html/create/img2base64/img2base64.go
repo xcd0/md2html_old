081025126b3204d4742e171726daa725cbc306e1
xcd0 <xcd0@xcd0.com>
2019-10-05 23:23:55 +0900

    大きな画像でbase64に変換した時シェルの1行文字数制限に引っかかる問題を雑に修正

---
+++ md2html/create/img2base64/img2base64.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"math"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+
+	/*
+		1: 入力テキストファイル
+		2: デコードさあれるファイルパス
+	*/
+	flag.Parse()
+	fmt.Fprintf(os.Stderr, "a0 %v\n", flag.Arg(0))
+	fmt.Fprintf(os.Stderr, "a1 %v\n", flag.Arg(1))
+
+	// ファイルをOpenする
+	f, err := os.Open(flag.Arg(0))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "File %s could not read: %v\n", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	// <img src="./build_on_win10.gif" alt="" /></p>
+	output := ""
+
+	scanner := bufio.NewScanner(f)
+	scanner.Buffer([]byte{}, math.MaxInt64)
+
+	input1 := "<img src="
+	input2 := flag.Arg(1)
+	for scanner.Scan() {
+		// ここで一行ずつ処理
+		line := scanner.Text()
+		b1 := strings.Index(line, input1)
+		b2 := strings.Index(line, input2)
+		if b1 == -1 {
+			output += line + "\n"
+		} else if b2 == -1 {
+			output += line + "\n"
+		} else {
+			base64code := encode(flag.Arg(1))
+			ext := filepath.Ext(flag.Arg(1))
+			if ext == ".gif" {
+				base64codetag := "<img src=\"data:image/gif;base64," + base64code + "\">"
+				output += base64codetag + "\n"
+			} else if ext == ".png" {
+				base64codetag := "<img src=\"data:image/png;base64," + base64code + "\">"
+				output += base64codetag + "\n"
+			} else if ext == ".jpg" || ext == ".jpeg" {
+				base64codetag := "<img src=\"data:image/jpeg;base64," + base64code + "\">"
+				output += base64codetag + "\n"
+			} else {
+				output += line + "\n"
+			}
+		}
+	}
+	if err = scanner.Err(); err != nil {
+		// エラー処理
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(output)
+}
+
+//エンコード
+func encode(str string) string {
+
+	file, _ := os.Open(str)
+	defer file.Close()
+
+	fi, _ := file.Stat() //FileInfo interface
+	size := fi.Size()    //ファイルサイズ
+
+	data := make([]byte, size)
+	file.Read(data)
+
+	return base64.StdEncoding.EncodeToString(data)
+}
--- readme.md
+++ readme.md
@@ -48,7 +48,9 @@ html単体で画像を含む、つまり可搬性のある文書が生成でき�
 	* このhtmlファイルには画像が埋め込まれているのでhtml単体で扱うことができる。
 
 Windows10 MSYS2 での実行例 gif  
+<center>
 ![](./build_on_win10.gif)
+</center>
 
 ## 使用条件
 
