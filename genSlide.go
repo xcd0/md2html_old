c7941fc888a4cb5d99a0b243762ff612487e8b03
xcd0 <xcd0@xcd0.com>
2019-11-13 01:19:02 +0900

    update

--- genSlide.go
+++ genSlide.go
@@ -45,6 +45,8 @@ func MakePdfForSlide(fi *Fileinfo) { // {{{
 
 	// ページごとに分割したmdをhtmlに変換する
 
+	var bodies []string
+
 	page_num := info.absolute_page // 0から始まり実際のページ数で終了している
 	for i := 0; i < page_num; i++ {
 		// 1ページづつ
@@ -55,6 +57,8 @@ func MakePdfForSlide(fi *Fileinfo) { // {{{
 
 		// htmlのbodyを生成
 		body := Makebody(mdpath, fi.RImgPath)
+
+		bodies = append(bodies, body)
 
 		// 画像を置換してhtmlを作る
 		html := Makeheader(*fi, fi.Dpath+"markdown.css") + body + Makefooter()
@@ -80,6 +84,168 @@ func MakePdfForSlide(fi *Fileinfo) { // {{{
 		}
 	}
 
+	genSlideHtml(bodies, filepath.Join(fi.Dpath, fi.Basename+"_slide.html"))
+
+} // }}}
+
+func returnPreBody(maxpage int) string { // {{{
+	return `<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="stylesheet" type="text/css" href="./github-markdown.css" />
+	<script type="text/javascript" async
+	src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML" async>
+	</script>
+	<script type="text/x-mathjax-config">
+	MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$']]}});
+	</script>
+	<style type="text/css">
+	<!--
+body{font-family:Helvetica,arial,sans-serif;font-size:14px;line-height:1.6;padding:0;background-color:#fff;color:#333x}a{color:#4183c4;text-decoration:nonex}a.absent{color:#c00x}a.anchor{display:block;padding-left:30px;margin-left:-30px;cursor:pointer;position:absolute;top:0;left:0;bottom:0x}h1,h2,h3,h4,h5,h6{margin:20px 0 10px;padding:0;font-weight:700;-webkit-font-smoothing:antialiased;cursor:text;position:relativex}h1:first-child,h1:first-child+h2,h2:first-child,h3:first-child,h4:first-child,h5:first-child,h6:first-child{margin-top:0;padding-top:0x}h1:hover a.anchor,h2:hover a.anchor,h3:hover a.anchor,h4:hover a.anchor,h5:hover a.anchor,h6:hover a.anchor{text-decoration:nonex}h1 code,h1 tt,h2 code,h2 tt,h3 code,h3 tt,h4 code,h4 tt,h5 code,h5 tt,h6 code,h6 tt{font-size:inheritx}h1{font-size:30px;border-bottom:2px solid #bbb;margin-bottom:20pxx}h1,h2{color:#000x}h2{font-size:24px;border-bottom:1px solid #cccx}h3{font-size:18pxx}h4{font-size:1pcx}h5,h6{font-size:14pxx}h6{color:#777x}blockquote,dl,li,ol,p,pre,table,ul{margin:15px 0x}hr{border:0 0 0;color:#ccc;height:4px;padding:0x}a:first-child h1,a:first-child h2,a:first-child h3,a:first-child h4,a:first-child h5,a:first-child h6,body>h1:first-child,body>h1:first-child+h2,body>h2:first-child,body>h3:first-child,body>h4:first-child,body>h5:first-child,body>h6:first-child{margin-top:0;padding-top:0x}h1 p,h2 p,h3 p,h4 p,h5 p,h6 p{margin-top:0x}li p.first{display:inline-blockx}ol,ul{padding-left:30pxx}ol:first-child,ul:first-child{margin-top:0x}ol:last-child,ul:last-child{margin-bottom:0x}dl,dl dt{padding:0x}dl dt{font-size:14px;font-weight:700;font-style:italic;margin:15px 0 5pxx}dl dt:first-child{padding:0x}dl dt>:first-child{margin-top:0x}dl dt>:last-child{margin-bottom:0x}dl dd{margin:0 0 15px;padding:0 15pxx}dl dd>:first-child{margin-top:0x}dl dd>:last-child{margin-bottom:0x}blockquote{border-left:4px solid #ddd;padding:0 15px;color:#777x}blockquote>:first-child{margin-top:0x}blockquote>:last-child{margin-bottom:0x}table{padding:0;border-spacing:2px;border-collapse:collapsex}table,td,th{border:1px solid #cccx}td,th{padding:0;margin:0}table tr{background-color:#fff;border-top:1px solid #c6cbd1;margin:0;padding:0x}table tr:nth-child(2n){background-color:#f6f8fax}table tr th{font-weight:700x}table tr td,table tr th{border:1px solid #ccc;text-align:left;margin:0;padding:6px 13pxx}table tr td:first-child,table tr th:first-child{margin-top:0x}table tr td:last-child,table tr th:last-child{margin-bottom:0x}img{max-width:100%;max-height:80%}span.frame,span.frame>span{display:block;overflow:hiddenx}span.frame>span{border:1px solid #ddd;float:left;margin:13px 0 0;padding:7px;width:autox}span.frame span img{display:block;float:leftx}span.frame span span{clear:both;color:#333;display:block;padding:5px 0 0x}span.align-center{display:block;overflow:hidden;clear:bothx}span.align-center>span{display:block;overflow:hidden;margin:13px auto 0;text-align:centerx}span.align-center span img{margin:0 auto;text-align:centerx}span.align-right{display:block;overflow:hidden;clear:bothx}span.align-right>span{display:block;overflow:hidden;margin:13px 0 0;text-align:rightx}span.align-right span img{margin:0;text-align:rightx}span.float-left{display:block;margin-right:13px;overflow:hidden;float:leftx}span.float-left span{margin:13px 0 0x}span.float-right{display:block;margin-left:13px;overflow:hidden;float:rightx}span.float-right>span{display:block;overflow:hidden;margin:13px auto 0;text-align:rightx}code,tt{margin:0 2px;padding:0 5px;white-space:nowrap;border:1px solid #eaeaea;background-color:#f8f8f8;border-radius:3pxx}pre code{margin:0;padding:0;white-space:pre;border:0;background:transparentx}.highlight pre,pre{background-color:#f8f8f8;border:1px solid #ccc;font-size:13px;line-height:19px;overflow:auto;padding:6px 10px;border-radius:3px}pre code,pre tt{background-color:transparent;border:0x}.sub{position:absolute}.main-content{max-width:100%;max-height:100%;margin:auto;padding:50px}hr{page-break-before:always;page-break-after:always;border:0!important;color:#fff;height:4px;padding:0}
+	-->
+	</style>
+<script type="text/javascript">
+<!--
+
+function goto(to_page) {
+	var page = document.getElementById(current);
+	page.style.visibility = "hidden";
+	current = to_page;
+	page = document.getElementById(current);
+	page.style.visibility = "visible";
+}
+
+function resize() {
+	//document.documentElement.clientWidth
+	//document.documentElement.clientHeight
+
+	var c1 = document.getElementById('container');
+	var h = window.innerHeight;
+	var w = window.innerWidth;
+	h = (h - 100) * 0.9;
+	w = (w - 100) * 0.9;
+	c1.style.Width= w + 'px';
+	c1.style.Height = h + 'px';
+
+	/*
+	var c document.getElementById(current);
+	var tags c.getElementByTagName("img");
+	for(var i = 0; i < tags.length; i++){
+		tag[i].style.Width = w + 'px';
+		tag[i].style.Height = h + 'px';
+	}
+	*/
+}
+
+var current_num;
+var current;
+function zeroPadding(num,length){
+	return ('0000000000' + num).slice(-length);
+}
+function main() {
+	resize();
+	current_num = zeroPadding(0,4);
+	current = "p" + String(current_num);
+	goto(current);
+}
+window.onload = main;
+
+
+window.addEventListener('load', resize, false);
+window.addEventListener('resize', resize, false);
+
+function prev(){
+
+	var page = document.getElementById(current);
+	page.style.visibility = "hidden";
+
+	if( current_num == "0000" ){
+		current_num = zeroPadding( ` + fmt.Sprintf("%04d", maxpage-1) + ` ,4);
+	} else {
+		current_num = zeroPadding( Number(current_num) - 1 ,4);
+	}
+	current = "p" + current_num;
+
+	page = document.getElementById(current);
+	page.style.visibility = "visible";
+}
+function next(){
+	var page = document.getElementById(current);
+	page.style.visibility = "hidden";
+
+	if( current_num == "` + fmt.Sprintf("%04d", maxpage-1) + `" ){
+		current_num = "0000"
+		current_num = zeroPadding( Number(current_num) + 1 ,4);
+	} else {
+		current_num = zeroPadding( Number(current_num) + 1 ,4);
+	}
+	current = "p" + current_num;
+
+	page = document.getElementById(current);
+	page.style.visibility = "visible";
+}
+window.addEventListener("click", function(e) {
+	console.log("offset:" + e.offsetX + "," + e.offsetY); 
+});
+
+var mousewheelevent = 'onwheel' in document ? 'wheel' : 'onmousewheel' in document ? 'mousewheel' : 'DOMMouseScroll';
+try{
+	document.addEventListener (mousewheelevent, onWheel, false);
+}catch(e){
+	//for legacy IE
+	document.attachEvent ("onmousewheel", onWheel);
+}
+function onWheel(e) {
+	if(!e) e = window.event; //for legacy IE
+	var delta = e.deltaY ? -(e.deltaY) : e.wheelDelta ? e.wheelDelta : -(e.detail);
+	if (delta < 0){
+		e.preventDefault();
+		//下にスクロールした場合の処理
+		next();
+	} else if (delta > 0){
+		e.preventDefault();
+		//上にスクロールした場合の処理
+		prev();
+	}
+}
+
+//-->
+</script>
+</head>
+<body>
+<div id="container" class="main-content">
+`
+} // }}}
+
+func genSlideHtml(bodies []string, slidePath string) { // {{{
+
+	// bodyのみのsliceから1つにまとめたhtmlを生成する。
+	output := returnPreBody(len(bodies))
+	for i, b := range bodies {
+		output += fmt.Sprintf("\n<div id=\"p%04d\" id=\"container\" class=\"sub\" style=\"visibility: hidden;\">", i)
+		output += b
+		if i == 0 {
+			output += fmt.Sprintf("<p><a href=\"javascript:goto('p%04d')\">次へ</a></p>", i+1)
+		} else if i < len(bodies)-1 {
+			output += fmt.Sprintf("<p><a href=\"javascript:goto('p%04d')\">戻る | <a href=\"javascript:goto('p%04d')\">次へ</a></p>", i-1, i+1)
+		} else {
+			output += fmt.Sprintf("<p><a href=\"javascript:goto('p%04d')\">表紙へ</a></p>", 0)
+		}
+		output += "</div>"
+	}
+	output += "</div>\n</body>\n</html>\n"
+
+	output = delEmptyLine(output)
+
+	// スライド用htmlの1ページを出力する
+	if err := ioutil.WriteFile(slidePath, []byte(output), 0644); err != nil {
+		fmt.Fprintf(os.Stderr, "File %s could not open : %v\n", slidePath, err)
+		fmt.Println(err)
+		panic(err)
+	}
 } // }}}
 
 func readPreamble(info *Info) { // {{{1
@@ -175,6 +341,10 @@ func dividePage(info *Info) { // {{{
 		if _, err := os.Stat(tmpdir); os.IsNotExist(err) {
 			os.Mkdir(tmpdir, 0777)
 			//fmt.Printf("%vを作成しました", tmpdir)
+		}
+		tmpdir = filepath.Join(info.tmp_path, "body")
+		if _, err := os.Stat(tmpdir); os.IsNotExist(err) {
+			os.Mkdir(tmpdir, 0777)
 		}
 		tmpdir = filepath.Join(info.tmp_path, "html")
 		if _, err := os.Stat(tmpdir); os.IsNotExist(err) {
@@ -372,4 +542,18 @@ func parseMd(info *Info, fi *Fileinfo) { // {{{
 	// 最後の1ページを出力する
 	dividePage(info)
 
+} // }}}
+
+func delEmptyLine(in string) string { // {{{
+
+	output := ""
+
+	r := regexp.MustCompile(`^$`)
+	lines := strings.Split(in, "\n")
+	for _, line := range lines {
+		if r.MatchString(line) == false {
+			output += line + "\n"
+		}
+	}
+	return output
 } // }}}
