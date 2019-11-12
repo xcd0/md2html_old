c3019422dc5a43e152153caba09f0a99e9635310
xcd0 <xcd0@xcd0.com>
2019-11-12 18:21:54 +0900

    update

--- genSlide.go
+++ genSlide.go
@@ -5,181 +5,371 @@ import ( // {{{
 	"io/ioutil"
 	//"log"
 	"os"
+	"path/filepath"
 	"regexp"
 	"strconv"
 	"strings"
 	//"sync"
 	//"text/template"
+	//"github.com/xcd0/go-nkf"
 ) // }}}
 
-type Info struct {
-	width            float64
-	height           float64
-	flagSize         bool
-	page             int
-	flagPrintPageNum bool
-	footer           string
-	h1               string
-	h2               string
-	h3               string
-	output           string
-	line             string
-	path             string
-}
+type Info struct { // {{{
+	width               float64
+	height              float64
+	flagSize            bool
+	page                int
+	absolute_page       int
+	bool_print_page_num bool
+	bool_print_title    bool
+	//h1                  string // 廃止
+	h2          string
+	h3          string
+	footer      string
+	preamble    string
+	output      string
+	line        string
+	debug       bool
+	state_code  bool
+	tmp_path    string
+	state_title int // このページにh1~h3の表記があるか、あればその数値 なければ-1
+} // }}}
 
-func MakeHtmlForSlide(fi *Fileinfo) {
+func MakePdfForSlide(fi *Fileinfo) { // {{{
+	//fmt.Println("MakePdfForSlide")
 
-	fi.TmpPath = fi.Dpath + "/.tmp"
-	if f, err := os.Stat(fi.TmpPath); os.IsNotExist(err) || !f.IsDir() {
-		// ない
-		// 作る
-		if _, err := os.Stat(fi.TmpPath); os.IsNotExist(err) {
-			os.Mkdir(fi.TmpPath, 0777)
-		}
-	} else {
-		// ある
-		// 消す
-		if err := os.Remove(fi.TmpPath); err != nil {
+	var info Info
+
+	// マークダウンファイルを調べ、ページごとに分割する
+	parseMd(&info, fi)
+
+	// ページごとに分割したmdをhtmlに変換する
+
+	page_num := info.absolute_page // 0から始まり実際のページ数で終了している
+	for i := 0; i < page_num; i++ {
+		// 1ページづつ
+		htmlpath := filepath.Join(fi.Dpath, ".tmp", "html", fmt.Sprintf("%04d.html", i))
+		mdpath := filepath.Join(fi.Dpath, ".tmp", "md", fmt.Sprintf("%04d.md", i))
+
+		//fmt.Printf(">>> htmlpath = %v\n", htmlpath)
+
+		// htmlのbodyを生成
+		body := Makebody(mdpath, fi.RImgPath)
+
+		// 画像を置換してhtmlを作る
+		html := Makeheader(*fi, fi.Dpath+"markdown.css") + body + Makefooter()
+
+		// スライド用htmlの1ページを出力する
+		if err := ioutil.WriteFile(htmlpath, []byte(html), 0644); err != nil {
+			fmt.Fprintf(os.Stderr, "File %s could not open : %v\n", htmlpath, err)
 			fmt.Println(err)
-		}
-		// 作る
-		if _, err := os.Stat(fi.TmpPath); os.IsNotExist(err) {
-			os.Mkdir(fi.TmpPath, 0777)
+			panic(err)
 		}
 	}
 
-	// ファイル開く
-	f, err := os.Open(fi.Dpath)
-	if err != nil {
-		fmt.Println("error")
-	}
-	defer f.Close()
-
-	// 一気に全部読み取り
-	b, err := ioutil.ReadAll(f)
-	md := string(b)
-
-	// 改行で分ける
-	lines := strings.Split(md, "\n")
-
-	info := Info{
-		// 初期値A4
-		width:            841.89,
-		height:           595.28,
-		flagSize:         false,
-		page:             0,
-		flagPrintPageNum: true,
-		footer:           "",
-		h1:               "none",
-		h2:               "none",
-		h3:               "none",
-		output:           "",
-		line:             "",
-		path:             fi.TmpPath,
-	}
-
-	regH1 := regexp.MustCompile(`^# `)
-	regH2 := regexp.MustCompile(`^## `)
-	regH3 := regexp.MustCompile(`^### `)
-	for _, info.line = range lines { // 一行ずつ
-		//for i, line := range lines { // 一行ずつ
-		// <!-- $で始まっていたら独自記法
-		if strings.Contains(info.line, "<!-- $") {
-			preamble(&info)
-		} else if strings.Contains(info.line, "===") {
-			// mdを===で分割する
-			dividePage(&info)
-		} else {
-			if regH1.MatchString(info.line) {
-				info.h1 = info.line[2:]
+	// 中間ディレクトリを削除
+	if f, err := os.Stat(info.tmp_path); os.IsNotExist(err) || f.IsDir() {
+		// 存在するので消す
+		if info.debug == false {
+			// デバッグ中でないとき
+			if err := os.Remove(info.tmp_path); err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println("debug:中間ディレクトリを削除していません")
 			}
-			if regH2.MatchString(info.line) {
-				info.h2 = info.line[3:]
-			}
-			if regH3.MatchString(info.line) {
-				info.h3 = info.line[4:]
-			}
-			info.output += info.line + "\n"
 		}
 	}
 
-}
+} // }}}
 
-func preamble(info *Info) {
-	if info.flagSize == false {
-		tmp := strings.Index(info.line, "<!-- $width:\"")
+func readPreamble(info *Info) { // {{{1
+	if info.absolute_page == 0 { // 表紙ページのみで指定できる項目 {{{
+		if info.flagSize == false {
+			tmp := strings.Index(info.line, "<!-- $width:\"")
+			if tmp >= 0 {
+				info.width, _ = strconv.ParseFloat(info.line[tmp+len("<!-- $width:\""):strings.Index(info.line, "\" -->")], 64)
+				return
+			}
+			tmp = strings.Index(info.line, "<!-- $height:\"")
+			if tmp >= 0 {
+				info.height, _ = strconv.ParseFloat(info.line[tmp+len("<!-- $height:\""):strings.Index(info.line, "\" -->")], 64)
+				return
+			}
+		}
+		tmp := strings.Index(info.line, "<!-- $size:\"16:9\" -->")
 		if tmp >= 0 {
-			info.width, _ = strconv.ParseFloat(info.line[tmp+len("<!-- $width:\""):strings.Index(info.line, "\" -->")], 64)
+			info.flagSize = true
+			info.width = 841.89
+			info.height = 841.89 * 9.0 / 16.0
 			return
 		}
-		tmp = strings.Index(info.line, "<!-- $height:\"")
+		tmp = strings.Index(info.line, "<!-- $size:\"4:3\" -->")
 		if tmp >= 0 {
-			info.height, _ = strconv.ParseFloat(info.line[tmp+len("<!-- $height:\""):strings.Index(info.line, "\" -->")], 64)
+			info.flagSize = true
+			info.width = 841.89
+			info.height = 841.89 * 3.0 / 4.0
 			return
 		}
-	}
-	tmp := strings.Index(info.line, "<!-- $size:\"16:9\" -->")
-	if tmp >= 0 {
-		info.flagSize = true
-		info.width = 841.89
-		info.height = 841.89 * 9.0 / 16.0
-		return
-	}
-	tmp = strings.Index(info.line, "<!-- $size:\"4:3\" -->")
-	if tmp >= 0 {
-		info.flagSize = true
-		info.width = 841.89
-		info.height = 841.89 * 3.0 / 4.0
-		return
-	}
-	tmp = strings.Index(info.line, "<!-- $page_number:\"")
+	} // }}}
+
+	// どのページでも指定できる項目
+	// ページ内に表記するページ数
+	tmp := strings.Index(info.line, "<!-- $page_number:\"")
 	if tmp >= 0 {
 		info.page, _ = strconv.Atoi(info.line[tmp+len("<!-- $page_number:\"") : strings.Index(info.line, "\" -->")])
 		return
 	}
+	// ページ内にページ数を表記する
 	tmp = strings.Index(info.line, "<!-- $set_page_number:\"true\" -->")
 	if tmp >= 0 {
-		info.flagPrintPageNum = true
+		info.bool_print_page_num = true
 		return
 	}
+	// ページ内にページ数を表記しない
 	tmp = strings.Index(info.line, "<!-- $set_page_number:\"false\" -->")
 	if tmp >= 0 {
-		info.flagPrintPageNum = false
+		info.bool_print_page_num = false
 		return
 	}
+	// フッターを設定
 	tmp = strings.Index(info.line, "<!-- $footer:\"")
 	if tmp >= 0 {
 		return
 	}
-}
-
-func dividePage(info *Info) {
-
-	if info.page > 0 || info.flagPrintPageNum {
-		// ページ数を出力しやすいようにする
-		info.line += fmt.Sprintf("", info.page)
+	// 各ページにタイトルを表示する
+	tmp = strings.Index(info.line, "<!-- $title:\"true\" -->")
+	if tmp >= 0 {
+		info.bool_print_title = true
+		return
 	}
+	// 各ページにタイトルを表示しない
+	tmp = strings.Index(info.line, "<!-- $title:\"false\" -->")
+	if tmp >= 0 {
+		info.bool_print_title = false
+		return
+	}
+} // }}}1
+
+func dividePage(info *Info) { // {{{
+
+	//fmt.Printf("ap : %v", info.absolute_page)
+
+	if info.absolute_page == 0 {
+		// 表紙ページのみ
+		if f, err := os.Stat(info.tmp_path); os.IsNotExist(err) || f.IsDir() {
+			// ある
+			// 消す
+			if err := os.RemoveAll(info.tmp_path); err != nil {
+				fmt.Println(err)
+				if info.debug {
+					fmt.Println("debug:既存の中間ディレクトリを削除しました")
+				}
+			}
+		}
+		// 作る
+		if _, err := os.Stat(info.tmp_path); os.IsNotExist(err) {
+			os.Mkdir(info.tmp_path, 0777)
+			//fmt.Printf("%vを作成しました", info.tmp_path)
+		}
+		tmpdir := filepath.Join(info.tmp_path, "md")
+		if _, err := os.Stat(tmpdir); os.IsNotExist(err) {
+			os.Mkdir(tmpdir, 0777)
+			//fmt.Printf("%vを作成しました", tmpdir)
+		}
+		tmpdir = filepath.Join(info.tmp_path, "html")
+		if _, err := os.Stat(tmpdir); os.IsNotExist(err) {
+			os.Mkdir(tmpdir, 0777)
+		}
+		tmpdir = filepath.Join(info.tmp_path, "pdf")
+		if _, err := os.Stat(tmpdir); os.IsNotExist(err) {
+			os.Mkdir(tmpdir, 0777)
+		}
+		if info.debug {
+			//fmt.Println("debug:中間ディレクトリを作成しました")
+			//fmt.Println(info.tmp_path)
+		}
+	}
+
+	outputOnePage(info)
+
+	// 出力終わったので次のページ用に設定する
+	info.page++
+	info.absolute_page++
+	info.state_code = false
+	info.state_title = -1
+
+	// リセット
+	info.output = ""
+	info.preamble = ""
+
+} // }}}
+
+func outputOnePage(info *Info) { // {{{
+
+	// フッタ
+	info.line += fmt.Sprintf("<footer>%v</footer>", info.footer)
+
+	if info.absolute_page >= 0 || info.bool_print_page_num {
+		// ページ数
+	}
+
+	if info.absolute_page > 0 && info.bool_print_page_num {
+		// 最初のページではなく、ページ数を表示するように設定されていたら
+		info.line += fmt.Sprintf("<div class=\"page_num>%v\"</div>", info.page)
+	}
+
 	// ここまでを出力
-	//fmt.Printf( "replace %4d : %v -> %v\n%v\n %v\n", i, r[0], r[1], line, strings.Replace(line, r[0], r[1], 1))
-	filename := fmt.Sprintf("%v/%04d.md", info.path, info.page)
-	file, err := os.Create(filename)
+
+	tmpfilename := filepath.Join(info.tmp_path, "md", fmt.Sprintf("%04d.md", info.absolute_page))
+
+	/*
+		if info.debug {
+			fmt.Println(">> " + tmpfilename)
+		}
+	*/
+
+	file, err := os.Create(tmpfilename)
 	defer file.Close()
 	if err != nil {
 		panic(err)
 	}
 
-	err = ioutil.WriteFile(filename, []byte(info.output), 0666)
+	switch info.state_title {
+	case 2: // このページ内に ## があった 引き継がない。
+		info.h2 = "none"
+		fallthrough
+	case 3: // このページ内に ### があった 引き継がない。
+		info.h3 = "none"
+	default:
+		// 何もしない
+	}
+
+	// h2 と h3のみ前のページから引き継いで表記ことができる
+	title := ""
+	if info.bool_print_title {
+		title += "\n<!-- 前のページから引き継いだタイトル ここから -->\n"
+		if info.h2 != "none" { // h2が指定されてなかったら何もしない
+
+			switch info.state_title {
+			case -1: // このページ内に ## と ### がなかった
+				// 前のページから引き継ぐ
+				title += "## " + info.h2 + "\n"
+				if info.h3 != "none" {
+					title += "### " + info.h3 + "\n"
+				}
+			case 2: // このページ内に ## があった 引き継がない。
+				// 何もしない
+			case 3: // このページ内に ### があった 引き継がない。
+				if info.h3 != "none" {
+					title += "## " + info.h2 + "\n"
+				}
+			default:
+				// 何もしない
+			}
+		}
+		title += "<!-- 前のページから引き継いだタイトル ここまで -->\n"
+	}
+
+	// 現在の設定を確認用に出力する 特に使わない
+	info.preamble += "<!-- 自動生成されたプリアンブル ここから -->\n"
+	info.preamble += fmt.Sprintf("<!-- // $width:\"%v\" -->\n", info.width)
+	info.preamble += fmt.Sprintf("<!-- // $height:\"%v\" -->\n", info.height)
+	info.preamble += fmt.Sprintf("<!-- // $page_number:\"%v\" -->\n", info.bool_print_page_num)
+	info.preamble += fmt.Sprintf("<!-- // $page:\"%v\" -->\n", info.page)
+	info.preamble += fmt.Sprintf("<!-- // $absolute_page:\"%v\" -->\n", info.absolute_page)
+	//info.preamble += fmt.Sprintf("<!-- // $h1:\"%v\" -->\n", info.h1)
+	info.preamble += fmt.Sprintf("<!-- // $h2:\"%v\" -->\n", info.h2)
+	info.preamble += fmt.Sprintf("<!-- // $h3:\"%v\" -->\n", info.h3)
+	info.preamble += fmt.Sprintf("<!-- // $title:\"%v\" -->\n", info.bool_print_title)
+	info.preamble += fmt.Sprintf("<!-- // $state_title:\"%v\" -->\n", info.state_title)
+	info.preamble += "<!-- 自動生成されたプリアンブル ここまで -->\n"
+
+	// プリアンブルとタイトルをくっつける
+	output := info.preamble + title + info.output
+
+	err = ioutil.WriteFile(tmpfilename, []byte(output), 0666)
 	if err != nil {
 		panic(err)
 	}
 
-	// リセット
+} // }}}
+
+func initInfo(info *Info, fi *Fileinfo) { // {{{
+	info.width = 841.89
+	info.height = 595.28
+	info.flagSize = false
+	info.page = 0          // これはページ内での表記に使用されるページ数
+	info.absolute_page = 0 // これは表記とは関係ないページ数
+	info.bool_print_page_num = true
+	info.bool_print_title = true // 各ページ毎にタイトルを表示するかどうか
+	//h1=                  "none" // 廃止
+	info.h2 = "none"
+	info.h3 = "none"
+	info.footer = ""
+	info.preamble = ""
 	info.output = ""
-	if info.page > 0 {
-		info.output += "<!-- h1:\"" + info.h1 + "\" -->"
-		info.output += "<!-- h2:\"" + info.h2 + "\" -->"
-		info.output += "<!-- h3:\"" + info.h3 + "\" -->"
-		info.output += "<!-- width:\"" + strconv.FormatFloat(info.width, 'f', 2, 64) + "\" -->"
-		info.output += "<!-- height:\"" + strconv.FormatFloat(info.height, 'f', 2, 64) + "\" -->"
+	info.line = ""
+	info.debug = true
+	info.state_code = false // これは```で囲まれている内側かどうかを保持する
+	info.tmp_path = ""      // .tmpフォルダの絶対パス
+	info.state_title = -1   // このページにh1~h3の表記があるか、あればその数値 なければ-1
+	info.tmp_path = filepath.Join(fi.Dpath, ".tmp")
+} // }}}
+
+func parseMd(info *Info, fi *Fileinfo) { // {{{
+
+	// 初期設定
+	initInfo(info, fi)
+
+	// 改行で分ける
+	lines := strings.Split(ReadMd(fi.Filename), "\n")
+
+	//regH1 := regexp.MustCompile(`^# `)
+	regH2 := regexp.MustCompile(`^## `)
+	regH3 := regexp.MustCompile(`^### `)
+	regCode := regexp.MustCompile("^```")
+	regPreamble := regexp.MustCompile(`^<!-- \$`)
+	regPageBreak := regexp.MustCompile(`^===$`)
+	for _, info.line = range lines { // 一行ずつ
+		//fmt.Printf(":%v", info.line)
+		//for i, line := range lines { // 一行ずつ
+		// <!-- $で始まっていたら独自記法
+		if regPreamble.MatchString(info.line) {
+			// マークダウンファイル内に記述されているプリアンブルを読み込む
+			readPreamble(info)
+		} else if regPageBreak.MatchString(info.line) {
+			// mdを===で分割する
+			// === は出力されない
+			dividePage(info)
+		} else {
+			// h2,h3を設定
+			if regH2.MatchString(info.line) {
+				// 上書き
+				info.h2 = info.line[3:]
+				info.state_title = 2
+				// h3はリセット
+				info.h3 = "none"
+			}
+			if regH3.MatchString(info.line) {
+				// 上書き
+				info.h3 = info.line[4:]
+				info.state_title = 3
+			}
+			if regCode.MatchString(info.line) {
+				// 論理反転
+				info.state_code = !info.state_code
+			}
+
+			if info.state_code == true {
+				// そのまま出力する ただし行末に半角空白を付与する
+				info.output += info.line + "  \n"
+			} else {
+				// ```で囲まれている場合は空白を入れない
+				info.output += info.line + "\n"
+			}
+		}
 	}
-}
+	// 最後の1ページを出力する
+	dividePage(info)
+
+} // }}}
--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
 import ( // {{{
-	"encoding/base64"
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"regexp"
 	//"log"
 	"os"
 	"path/filepath"
 	"sort"
 	"strings"
-	//"sync"
+	"sync"
 	//"text/template"
 ) // }}}
 
@@ -20,18 +20,29 @@ func main() {
 	// 引数を元に構造体を作る
 	fi := Argparse(flag.Arg(0))
 
-	// そのまま印刷したら単純な文書になる印刷用htmlを出力
-	makeHtml(&fi)
+	// wg4searchが終わるのを待ってwgをすすめる
+	wg := sync.WaitGroup{}
 
-	// 独自置換対象文字列
-	makeReplaceStrings(&fi)
+	// 画像を探す
+	searchTargetFile(&fi)
 
-	// 単純に生成した印刷用htmlに対して 置換などの処理を行った後出力する
-	// 独自拡張の置換と画像のbase64置換
-	filter2Html(&fi)
+	// そのまま印刷したら単純な文書になる印刷用htmlを作成する 出力はしない
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		//fmt.Println("call : makeHtmlNP")
+		makeHtml(&fi)
+	}()
 
 	// スライド用htmlを生成する
-	MakeHtmlForSlide(&fi)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		//fmt.Println("call : MakePdfForSlideNP")
+		MakePdfForSlide(&fi)
+	}()
+
+	wg.Wait()
 
 	// pdfをつくる
 	/*
@@ -42,280 +53,38 @@ func main() {
 
 func makeHtml(fi *Fileinfo) { // {{{
 
-	fi.Flavor = ""
-	fi.Html = Makehtml(fi)
+	fi.Html = Makeheader(*fi, "") + Makebody(fi.Apath, fi.RImgPath) + Makefooter()
 
-	//log.Println(fi.Html)
-
-	// 出力する
+	// 印刷用htmlを出力する
 	err := ioutil.WriteFile(fi.Htmlpath, []byte(fi.Html), 0644)
 	if err != nil {
 		// Openエラー処理
 		fmt.Fprintf(os.Stderr, "File %s could not open : %v\n", fi.Htmlpath, err)
-		fmt.Println(err)
-		return
+		//fmt.Println(err)
+		panic(err)
 	}
 
 } // }}}
 
-func filter2Html(fi *Fileinfo) { // {{{1
+func filter2body(in string) string { // {{{1
 
-	//csspath := fi.Dpath + "slide.css"
-	csspath := fi.Dpath + "markdown.css"
-	header := Makeheader(*fi, csspath)
-	body := Makebody(*fi)
-	footer := Makefooter()
+	lines := strings.Split(in, "\n")
 
-	// fi.Htmlを置換していく
-	// 改行で分割
-	fi.Html = header + body + footer
-	lines := strings.Split(fi.Html, "\n")
+	// 独自置換対象文字列
+	var rep [][]string
+	rep = append(rep, []string{`^===$`, "\n<div style='page-break-before:always'></div>\n"})
 
 	output := ""
-	for _, line := range lines { // 一行ずつ
-		//for i, line := range lines { // 一行ずつ
-		for _, r := range fi.Rep { // 全ての置換対象文字列について回す
-			if strings.Contains(line, r[0]) {
-				//fmt.Printf( "replace %4d : %v -> %v\n%v\n %v\n", i, r[0], r[1], line, strings.Replace(line, r[0], r[1], 1))
-				output += strings.Replace(line, r[0], r[1], 1) + "\n"
-			} else {
-				output += line + "\n"
-			}
+	for _, r := range rep { // 全ての置換対象文字列について回す
+		reg := regexp.MustCompile(r[0])
+		for _, line := range lines { // 一行ずつ
+			output += reg.ReplaceAllString(line, r[1]) + "\n"
 		}
 	}
 	// 上書きする
-	fi.Html = output
-
-	// 画像を上書きする
-	replaceImg(fi)
-
-	err := ioutil.WriteFile(fi.Htmlpath, []byte(fi.Html), 0644)
-	if err != nil {
-		// Openエラー処理
-		fmt.Fprintf(os.Stderr, "File %s could not open : %v\n", fi.Htmlpath, err)
-		fmt.Println(err)
-		panic(err)
-	}
+	return output
 
 } // }}}1
-
-func makeReplaceStrings(fi *Fileinfo) { // {{ {
-	// 独自拡張
-	fi.Rep = append(fi.Rep, []string{"===", "<div style='page-break-before:always'></div>"})
-} // }} }
-
-func replaceImg(fi *Fileinfo) { // {{{
-	// 画像の置き換え
-	// 画像を探す
-	outputList := searchTargetFile(*fi)
-	// これを文字列の長い順に並び変える
-	// でないと浅い階層に同じ名前のファイルがある場合誤った置換が発生する
-	outputList = sortStirngsLen(outputList)
-
-	output := "" // これが出力される
-
-	// ここで一行ずつ処理
-	lines := strings.Split(fi.Html, "\n")
-
-	for _, line := range lines { // 一行ずつ
-		//for j, line := range lines { // 一行ずつ
-		// <img src=が含まれるはずなので前もって判別しておく
-		if strings.Contains(line, "<img src=") == false {
-			// <img src=が含まれていない
-			output += line + "\n"
-		} else {
-			// <img src=が含まれる
-
-			// リストにある画像のパスが含まれるか調べる
-			for _, path := range outputList {
-				// もしoutputListにおいて先にマッチしていたら
-				// パスが\で区切られている場合を考えてすべて/にする
-				line = strings.Replace(line, "\\", "/", -1)
-				path = strings.Replace(path, "\\", "/", -1)
-				// チェック
-				if strings.Contains(line, path) == false {
-					// 違うので次のpathに
-					continue
-				}
-				// マッチ
-
-				//fmt.Printf("---- line : %v , match : %v ----\n%v\n", j, path, line)
-
-				// lineに含まれるパスの前後を切り出す
-				// 例
-				// <li><p><img src="./img/build_on_win10.gif" alt=""></p></li>
-				//                 ↑                       ↑
-				// このダブルクォーテーションの位置を調べる
-				// ./が混じらないよう 一文字づつ前方に調べて"を探す
-
-				// <img>にリンクが張ってあればそれを消す
-				tmpLinkNum := strings.Index(line, "<a")
-				if tmpLinkNum >= 0 {
-					tmpLinkNumPost := -1
-					for i := tmpLinkNum; i < len(line); i++ {
-						if line[i] == '>' {
-							// 見つかったのでそれをpostDQとする
-							tmpLinkNumPost = i + 1
-							break
-						}
-					}
-					if tmpLinkNumPost < 0 {
-						panic(1)
-					} else {
-
-						//fmt.Println("--->>\n" + line)
-						line = line[:tmpLinkNum] + line[tmpLinkNumPost:]
-						//fmt.Println("---<<\n" + line)
-					}
-					tmpLinkNum = strings.Index(line, "</a>")
-
-					if tmpLinkNum >= 0 {
-						//fmt.Println("--->>\n" + line)
-						line = line[:tmpLinkNum] + line[tmpLinkNum+len("</a>"):]
-						//fmt.Println("---<<\n" + line)
-					}
-				}
-
-				// <imgより前にマッチしないようにする
-				preDQ := -1
-				tmpNum := strings.Index(line, "<img")
-				tmpLine := line[tmpNum:]
-				//fmt.Println("---tmpline\n" + tmpLine)
-
-				for i := strings.Index(tmpLine, path); i >= 0; i-- {
-					if tmpLine[i] == '"' {
-						// 見つかったのでそれをpreDQとする
-						preDQ = tmpNum + i + 1
-						//fmt.Println("---match!\n" + line[:preDQ])
-						break
-					}
-				}
-				if preDQ < 0 {
-					// ダブルクォーテーションが見つからなかった
-					fmt.Println("マッチしましたが前にダブルクォーテーションがありませんでした")
-					output += line + "\n"
-					break
-				}
-
-				postDQ := -1
-				for i := preDQ; i < len(line); i++ {
-					if line[i] == '"' {
-						// 見つかったのでそれをpostDQとする
-						postDQ = i
-						break
-					}
-				}
-				if postDQ < 0 {
-					// ダブルクォーテーションが見つからなかった
-					fmt.Println("マッチしましたが後ろにダブルクォーテーションがありませんでした")
-					/*
-						fmt.Println("ori " + line)
-						fmt.Println("cut pre  :" + line[preDQ:])
-						fmt.Println("cut post :" + line[:])
-					*/
-					output += line + "\n"
-					break
-				}
-
-				//
-				/*
-					fmt.Println("<<\n" + line)
-					fmt.Printf(">> pre : %v '%v', post : %v '%v'\n", preDQ, line[preDQ], postDQ, line[postDQ])
-					fmt.Printf(">> %v\n", line[preDQ:postDQ])
-
-					code := "@@@@@@"
-					/ */
-				///*
-				// 文字列が含まれているかどうか調べる
-				// base64でエンコードしたデータに置き換える
-				base64code := EncodeBase64(path)
-				// ファイルの拡張子ごとにヘッダをつける
-				// gif,png,jpg,jpegのみ
-				ext := filepath.Ext(path) // 一致した画像ファイルのパスから拡張子を調べる
-				code := ""
-				if ext == ".gif" {
-					code = "data:image/gif;base64," + base64code
-				} else if ext == ".png" {
-					code = "data:image/png;base64," + base64code
-				} else if ext == ".jpg" || ext == ".jpeg" {
-					code = "data:image/jpeg;base64," + base64code
-				}
-
-				// */
-				// 前後を切り出す
-				pre := line[:preDQ]
-				post := line[postDQ:]
-
-				// くっつけて上書きする
-				line = pre + code + post
-
-				//fmt.Println(">>\n" + line)
-
-				// 置換したらその行は終わる
-				break
-
-			}
-			// すべてのpathをチェックした
-			//fmt.Println(line)
-			// 出力する
-			output += line + "\n"
-		}
-	}
-
-	// 上書き
-	fi.Html = output
-} // }}}
-
-func searchTargetFile(fi Fileinfo) []string { // {{{1
-
-	outputList := []string{}
-
-	filepath.Walk(fi.Dpath, func(path string, _ os.FileInfo, _ error) error {
-		// 相対パスを取得
-		relativePath, _ := filepath.Rel(fi.Dpath, path)
-
-		// jpg,jpeg,png,gif
-		ext := filepath.Ext(relativePath)
-		if ext == ".jpg" || ext == ".jpeg" || ext == ".png" || ext == ".gif" {
-			fmt.Println(relativePath)
-			outputList = append(outputList, relativePath)
-		}
-		return nil
-	})
-
-	return outputList
-} // }}}1
-
-/*
-func replace(input, targetArray [][]string) (string, error) { // {{{
-
-	lines := strings.Split(input, "\n")
-
-	// 全ての置換対象文字列について回す
-	for i := 0; i < len(targetArray); i++ {
-
-		output := strings.NewRreplacer().Replace(lines)
-
-	}
-} // }}}
-*/
-//エンコード
-func EncodeBase64(str string) string { // {{{
-
-	file, err := os.Open(str)
-	defer file.Close()
-	if err != nil {
-		panic(err)
-	}
-
-	fi, _ := file.Stat() //FileInfo interface
-	size := fi.Size()    //ファイルサイズ
-
-	data := make([]byte, size)
-	file.Read(data)
-
-	return base64.StdEncoding.EncodeToString(data)
-} // }}}
 
 func sortStirngsLen(in []string) []string { // {{{
 
@@ -338,3 +107,28 @@ func sortStirngsLen(in []string) []string { // {{{
 	}
 	return out
 } // }}}
+
+func searchTargetFile(fi *Fileinfo) { // {{{1
+	//fmt.Println("searchTargetFile")
+
+	outputList := []string{}
+
+	filepath.Walk(fi.Dpath, func(path string, _ os.FileInfo, _ error) error {
+		// 相対パスを取得
+		relativePath, _ := filepath.Rel(fi.Dpath, path)
+
+		// jpg,jpeg,png,gif
+		ext := filepath.Ext(relativePath)
+		if ext == ".jpg" || ext == ".jpeg" || ext == ".png" || ext == ".gif" {
+			//fmt.Println(relativePath)
+			outputList = append(outputList, relativePath)
+		}
+		return nil
+	})
+
+	// これを文字列の長い順に並び変える
+	// でないと浅い階層に同じ名前のファイルがある場合誤った置換が発生する
+	outputList = sortStirngsLen(outputList)
+
+	fi.RImgPath = outputList
+} // }}}1
